a4481d9049f0c1e7be51e38dcde9f2ba65bfa4ca
shristivyas <77220498+shristivyas@users.noreply.github.com>
2022-03-01 07:46:31 +0530

    Create pattern3.cpp

---
+++ pattern3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i=1;
+    
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout << i;
+            j+=1;
+            }
+        cout << endl;
+        i+=1;
+            
+        
+    }
+
+    return 0;
+}
+
+
+/* output:
+n=3
+1
+22
+333
+*/
